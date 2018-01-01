d8ba33763be4955062f72af81b6c969911fee9a3
Forge <github@forgethepath.com>
2018-01-01 16:26:43 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+"use strict";
+require('dotenv').config()
+
+const algoliasearch = require('algoliasearch');
+const axios = require('axios');
+
+const appid = process.env.ALGOLIA_APPID;
+const token = process.env.ALGOLIA_TOKEN;
+const client = algoliasearch(appid, token);
+const index  = client.initIndex('forge_search');
+const endpoint = 'https://forge-blog.netlify.com/index.json'
+
+const getForgeData = async () => {
+  try {
+    const response = await axios.get(endpoint);
+    return response.data.items;
+  } catch (e) {
+    console.error(e); 
+  }
+};
+
+const updateAlgolia = async () => {
+  try {
+    getForgeData().then(result => index.addObjects(result));
+  } catch (e) {
+    console.error(e); 
+  }
+};
+
+updateAlgolia();
---
+++ readme.md
@@ -0,0 +1,7 @@
+### **Forge Search** - powered by Algolia.
+
+```
+Index's JSON from forge-blog into Algolia. Currently in development.
+```
+
+Please credit original author if code or theme is useful for a project of your own.
